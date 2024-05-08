00fab9eb76941c5b5321a75856e38fff82fd6e7d
Boris Vlasenko <boris-vlasenko@yandex.ru>
2024-05-08 14:37:14 +0300

    нечто

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth import get_user_model
