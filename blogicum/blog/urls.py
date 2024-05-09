bc0650e527d33a3b78296a57c26ce9f0efc51a60
Boris Vlasenko <boris-vlasenko@yandex.ru>
2024-05-09 11:48:46 +0300

    asdf

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
     path('posts/<int:pk>/edit/', views.create_post, name='edit_post'),
     path('profile/<str:username>/', views.profile, name='profile'),
     path('edit_profile/', views.edit_profile, name='edit_profile'),
-    path('post/<int:pk>/comment/', views.add_comment, name='add_comment'),
+    path('posts/<int:pk>/comment/', views.add_comment, name='add_comment'),
 
-    path('post/<int:pk>/edit_comment/<int:comment_pk>/',
+    path('posts/<int:pk>/edit_comment/<int:comment_pk>/',
          views.edit_comment, name='edit_comment'),
 
     path('post/<int:pk>/delete_comment/<int:comment_pk>/',
